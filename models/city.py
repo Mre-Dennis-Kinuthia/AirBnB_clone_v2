05291ee5e3eace8c13c902e167aed6f178d34754
Mre-Dennis-Kinuthia <Denniskinuthia1105@gmail.com>
2023-04-02 18:58:17 +0300

    init

--- models/city.py
+++ models/city.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+"""
+City Module
+"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """City class handles all application cities"""
+    __tablename__ = "cities"
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="cities",
+                          cascade="all, delete-orphan")
+
+    def __init__(self, *args, **kwargs):
+        """initialize City"""
+        super().__init__(*args, **kwargs)
--- models/user.py
+++ models/user.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""
+User Module
+"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+    """User class handles all application users"""
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user",
+                          cascade="all, delete-orphan")
 
     def __init__(self, *args, **kwargs):
-        """Initializes a new User"""
+        """initialize User"""
         super().__init__(*args, **kwargs)
